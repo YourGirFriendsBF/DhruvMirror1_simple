6231b50824c3ab88d1003e32230b7f2bdf165fc2
YourGirFriendsBF <109566119+YourGirFriendsBF@users.noreply.github.com>
2022-08-05 01:23:07 +0530

    Update split_status.py

--- bot/helper/mirror_utils/status_utils/split_status.py
+++ bot/helper/mirror_utils/status_utils/split_status.py
@@ -9,10 +9,16 @@ class SplitStatus:
 
     def progress(self):
         return '0'
+        return f'{round(self.progress_raw(), 2)}%'
 
     def speed(self):
         return '0'
-
+        return f'{get_readable_file_size(self.speed_raw())}/s'def speed(self):
+        return '0'
+        return f'{get_readable_file_size(self.speed_raw())}/s'
+    def size_raw(self):
+        return self.__size
+    
     def name(self):
         return self.__name
 
@@ -24,6 +30,11 @@ class SplitStatus:
 
     def eta(self):
         return '0s'
+        try:
+            seconds = (self.size_raw() - self.processed_bytes()) / self.speed_raw()
+            return f'{get_readable_time(seconds)}'
+        except:
+            return '-'
 
     def status(self):
         return MirrorStatus.STATUS_SPLITTING
